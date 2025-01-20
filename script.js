beede48569c531ac78ac00dc1c8fae024ad3fa46
Jitesh Kumar <105545377+jitesh-20@users.noreply.github.com>
2025-01-20 15:42:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstChar(text) {
   // your code here
+	if(text==' ' || text==''){
+		return '';
+	}
+	let str=text.trim();
+	return str[0];
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(firstChar(text));
+
+ 
